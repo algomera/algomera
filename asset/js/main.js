ec20babe3ea94c9bfe0087db05f7c1c5127a1db2
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2022-06-13 11:59:16 +0200

    create landing algomera

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+
+function menu() {
+    let m = document.getElementById('nav-icon');
+     m.classList.toggle('open');
+
+    let o = document.getElementById('menu');
+    o.classList.toggle('visible');
+};
+
